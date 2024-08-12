2ba8574e33be60f2b57f109b6cb7c749f21b4242
Bhabya Sinha <sinhabhabya.97@gmail.com>
2024-08-11 23:15:58 -0400

    get song by title

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import { Table } from './Table'
-
 
 const Dashboard = () => {
   const [songs, setSongs] = useState([])
   const [page, setPage] = useState(1)
   const [totalPages, setTotalPages] = useState(1)
-
- 
+  const title = useRef(null);
+  const [songByTitle, setSongByTitle] = useState([])
+  const [showByTitle, setShowByTitle] = useState(false)
+  const [showTable, setShowTable] = useState(true)
 
   useEffect(function() {
       fetch(`/songs?page=${page}&per_page=10`).then(
@@ -19,9 +20,20 @@ const Dashboard = () => {
         }
       )
     
-  }, [page]);
+  }, [page, showTable]);
 
-
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setShowByTitle(true)
+    setShowTable(false)
+    fetch(`/searchByTitle?title=${encodeURIComponent(title.current.value)}`).then(
+      (res) => res.json()
+    ).then(
+      data => {
+        setSongByTitle(data)
+      }
+    )
+  }
   const handleNextPage = (e) => {
     e.preventDefault();
     if (page < totalPages) {
@@ -36,11 +48,41 @@ const Dashboard = () => {
     }  
   }
 
+  const handleShowButtonClick = (e) => {
+    e.preventDefault()
+    setShowTable(true)
+    setShowByTitle(false)
+    setPage(1)
+    title.current.value = ''
+  }
+ 
   return (
     <>
-        <div className="bg-blue-100 flex justify-center h-15">
-            <h1 className="text-3xl font-bold my-5">Vivpro Assignment: Bhabya Sinha</h1>
+      <div className="bg-blue-100 flex justify-center h-15">
+        <h1 className="text-3xl font-bold my-5">Vivpro Assignment: Bhabya Sinha</h1>
+      </div>
+      <div>
+        <form className="my-2" onSubmit={handleSearch}>
+          <input
+            ref={title}
+            type="text"
+            placeholder="Search By title"
+            className="ml-2 p-2 border border-gray-300"
+          />
+          <button type="submit" className="p-2 bg-blue-300 ml-2">
+            Get Song
+          </button>
+          <button className="p-2 bg-blue-300 ml-6" onClick={handleShowButtonClick}>
+            Show all songs
+          </button>
+        </form>
+      </div>
+      {showByTitle && songByTitle && (
+        <div className="mx-24 mb-16 my-16">
+          <Table songsData={songByTitle} />
         </div>
+      )}
+      {showTable && songs && (
         <div className="mx-24 mb-16 my-16">
           <Table songsData={songs} />
           <div className="flex justify-center items-center mt-1 p-1 bg-white border-t">
@@ -49,7 +91,7 @@ const Dashboard = () => {
               disabled={page === 1}
               className="px-3 py-1 bg-gray-300 rounded disabled:opacity-50 mx-2"
             >
-                Previous
+              Previous
             </button>
             <span className="text-sm">
               Page {page} of {totalPages}
@@ -59,10 +101,11 @@ const Dashboard = () => {
               disabled={page === totalPages}
               className="px-3 py-1 bg-gray-300 rounded disabled:opacity-50 mx-2"
             >
-                Next
+              Next
             </button>
           </div>
         </div>
+      )}
     </>
   );
 };
